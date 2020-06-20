320a06dabd9da983329be960b6760efa7385e1be
calvin <puram.calvin@gmail.com>
2020-06-20 17:10:05 +0100

    setup create form local state

--- client/src/components/profileForms/CreateProfile.js
+++ client/src/components/profileForms/CreateProfile.js
@@ -2,6 +2,45 @@ import React, { Fragment, useState } from 'react';
 
 const CreateProfile = () => {
   const [toggleSocial, setToggleSocial] = useState(false);
+  const [formData, setFormData] = useState({
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    githubusername: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: ''
+  });
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const {
+    company,
+    website,
+    location,
+    status,
+    skills,
+    githubusername,
+    bio,
+    twitter,
+    facebook,
+    linkedin,
+    youtube,
+    instagram
+  } = formData;
+
   return (
     <Fragment>
       <h1 className="large text-primary">Create Your Profile</h1>
@@ -10,9 +49,9 @@ const CreateProfile = () => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-group">
-          <select name="status">
+          <select name="status" value={status} onChange={handleChange}>
             <option value="0">* Select Professional Status</option>
             <option value="Developer">Developer</option>
             <option value="Junior Developer">Junior Developer</option>
@@ -28,25 +67,49 @@ const CreateProfile = () => {
           </small>
         </div>
         <div className="form-group">
-          <input type="text" placeholder="Company" name="company" />
+          <input
+            type="text"
+            placeholder="Company"
+            name="company"
+            value={company}
+            onChange={handleChange}
+          />
           <small className="form-text">
             Could be your own company or one you work for
           </small>
         </div>
         <div className="form-group">
-          <input type="text" placeholder="Website" name="website" />
+          <input
+            type="text"
+            placeholder="Website"
+            name="website"
+            value={website}
+            onChange={handleChange}
+          />
           <small className="form-text">
             Could be your own or a company website
           </small>
         </div>
         <div className="form-group">
-          <input type="text" placeholder="Location" name="location" />
+          <input
+            type="text"
+            placeholder="Location"
+            name="location"
+            value={location}
+            onChange={handleChange}
+          />
           <small className="form-text">
-            City & state suggested (eg. Boston, MA)
+            City & state suggested (eg. Ibadan, Oyo)
           </small>
         </div>
         <div className="form-group">
-          <input type="text" placeholder="* Skills" name="skills" />
+          <input
+            type="text"
+            placeholder="* Skills"
+            name="skills"
+            value={skills}
+            onChange={handleChange}
+          />
           <small className="form-text">
             Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)
           </small>
@@ -56,6 +119,8 @@ const CreateProfile = () => {
             type="text"
             placeholder="Github Username"
             name="githubusername"
+            value={githubusername}
+            onChange={handleChange}
           />
           <small className="form-text">
             If you want your latest repos and a Github link, include your
@@ -63,7 +128,12 @@ const CreateProfile = () => {
           </small>
         </div>
         <div className="form-group">
-          <textarea placeholder="A short bio of yourself" name="bio"></textarea>
+          <textarea
+            placeholder="A short bio of yourself"
+            name="bio"
+            value={bio}
+            onChange={handleChange}
+          ></textarea>
           <small className="form-text">Tell us a little about yourself</small>
         </div>
 
@@ -82,27 +152,57 @@ const CreateProfile = () => {
           <Fragment>
             <div className="form-group social-input">
               <i className="fab fa-twitter fa-2x"></i>
-              <input type="text" placeholder="Twitter URL" name="twitter" />
+              <input
+                type="text"
+                placeholder="Twitter URL"
+                name="twitter"
+                value={twitter}
+                onChange={handleChange}
+              />
             </div>
 
             <div className="form-group social-input">
               <i className="fab fa-facebook fa-2x"></i>
-              <input type="text" placeholder="Facebook URL" name="facebook" />
+              <input
+                type="text"
+                placeholder="Facebook URL"
+                name="facebook"
+                value={facebook}
+                onChange={handleChange}
+              />
             </div>
 
             <div className="form-group social-input">
               <i className="fab fa-youtube fa-2x"></i>
-              <input type="text" placeholder="YouTube URL" name="youtube" />
+              <input
+                type="text"
+                placeholder="YouTube URL"
+                name="youtube"
+                value={youtube}
+                onChange={handleChange}
+              />
             </div>
 
             <div className="form-group social-input">
               <i className="fab fa-linkedin fa-2x"></i>
-              <input type="text" placeholder="Linkedin URL" name="linkedin" />
+              <input
+                type="text"
+                placeholder="Linkedin URL"
+                name="linkedin"
+                value={linkedin}
+                onChange={handleChange}
+              />
             </div>
 
             <div className="form-group social-input">
               <i className="fab fa-instagram fa-2x"></i>
-              <input type="text" placeholder="Instagram URL" name="instagram" />
+              <input
+                type="text"
+                placeholder="Instagram URL"
+                name="instagram"
+                value={instagram}
+                onChange={handleChange}
+              />
             </div>
           </Fragment>
         )}
