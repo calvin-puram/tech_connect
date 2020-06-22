5f98dfad5ad155084ecca4227ef54607c0a16f4b
calvin <puram.calvin@gmail.com>
2020-06-22 20:49:13 +0100

    preparing for deployment

--- server.js
+++ server.js
@@ -60,11 +60,12 @@ app.use('/api/v1/posts', postsRoutes);
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'public')));
+  // Set static folder
+  app.use(express.static('client/build'));
 
-  app.get('*', (req, res, next) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-  );
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
