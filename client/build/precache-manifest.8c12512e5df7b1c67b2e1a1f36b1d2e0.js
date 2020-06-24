2f62511b46ac439d13a1bc3950e8e01ab86a02ea
calvin <puram.calvin@gmail.com>
2020-06-24 21:43:52 +0100

    ready for deployment

---
+++ client/build/precache-manifest.8c12512e5df7b1c67b2e1a1f36b1d2e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5181b84d25ab12d0344a306bc33a9417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23948f836a88045cbdee",
+    "url": "/static/css/main.386864f0.chunk.css"
+  },
+  {
+    "revision": "cbf67f411501a2c9c273",
+    "url": "/static/js/2.938008b8.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.938008b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23948f836a88045cbdee",
+    "url": "/static/js/main.fa1afd3b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
